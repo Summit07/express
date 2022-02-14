2683a320fba51712f5b3d80dc675d96f2a83921e
Summit Gupta <summitgupta07@gmail.com>
2022-02-14 11:55:37 +0530

    rest api for express Goal

--- backend/controllers/goalcontrollers.js
+++ backend/controllers/goalcontrollers.js
@@ -1,23 +1,54 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../modals/goalsModal");
 
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `get new Goals` });
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
 });
+
 const setGoals = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("please new error detected");
   }
-  res.status(200).json({ message: `Create new Goals` });
+  const goal = await Goal.create({ text: req.body.text });
+  res.status(200).json(goal);
 });
+
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update Goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not Found");
+  }
+  const updatedGoal = await Goal.findById(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
+  // res.status(200).json({ message: `Update Goal ${req.params.id}` });
 });
+
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal is not found");
+  }
+  await goal.remove(req.params.id);
+  res.status(200).json({ message: `Deleted Goal ${req.params.id}` });
 });
+
 const patchUpGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Patch update Goals  ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Patch Goal not Found");
+  }
+  const patchUpdatedGoal = await Goal.findById(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(patchUpdatedGoal);
 });
 
 module.exports = { getGoals, setGoals, updateGoals, deleteGoals, patchUpGoals };
